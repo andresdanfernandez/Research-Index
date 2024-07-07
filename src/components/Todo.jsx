fa32bb439cf145cd4cc3131c603f0d517b657528
Andres Fernandez <46586316+andyf9903@users.noreply.github.com>
2024-07-07 16:07:46 -0400

    add text styling to cards

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,12 +5,12 @@ function Todo(props) {
     return (
         <div className="scrollable-div overflow-auto bg-secondary place-self-center w-11/12 max-w-md flex flex-col p-7 max  max-h-96 rounded-xl">
             <div className="flex justify-between">
-                <h1 className="text-2xl  block text-slate-300">{props.title}</h1>
+                <h1 className="text-2xl font-bold block text-slate-300">{props.title}</h1>
                 <div className="flex justify-center items-center bg-stone-900 hover:bg-stone-800 rounded text-slate-300" >
                     {props.title === "Add Papers" && (button)}
                 </div>
             </div>
-            <div className="text-field pt-1 text-slate-300" >
+            <div className="text-field pt-1 text-md text-slate-300" >
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum hic iure voluptates reiciendis ab eveniet consequuntur, odio odit commodi ratione? Eos repudiandae iusto magnam atque iure sequi obcaecati quia fuga!</p>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum hic iure voluptates reiciendis ab eveniet consequuntur, odio odit commodi ratione? Eos repudiandae iusto magnam atque iure sequi obcaecati quia fuga!</p>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum hic iure voluptates reiciendis ab eveniet consequuntur, odio odit commodi ratione? Eos repudiandae iusto magnam atque iure sequi obcaecati quia fuga!</p>
