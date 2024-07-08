c331fbe4d566cdb3d19f81479f668d2a7fd72fa1
Andres Fernandez <46586316+andyf9903@users.noreply.github.com>
2024-07-08 13:24:17 -0400

    add field component

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,6 +1,10 @@
-const button = <button className=" pr-3 pl-3 text-sm hover:bg-stone-800 ">Add</button>;
+import {useState} from "react";
+import Field from "./Field";
+
+const button = <button className="pr-3 pl-3 text-sm hover:bg-stone-800 ">Add</button>;
 
 function Card(props) {
+
     return (
             <div className="scrollable-div overflow-auto bg-secondary place-self-center w-11/12 max-w-md flex flex-col p-7 h-96 rounded-xl">
                 <div className="flex justify-between">
@@ -9,14 +13,12 @@ function Card(props) {
                         {props.title === "Add Papers" && button}
                     </div>
                 </div>
-                <div className="flex flex-col gap-2 text-field pt-2 text-sm text-slate-300" >
-                    <div className="paper border-2" >
-                        <h2>Example URL</h2>
-                    </div>
-                    <div className="paper border-2" >
-                        <h2>Example URL</h2>
-                    </div>
+
+                <div className="flex flex-col gap-2 pt-2 text-sm text-slate-300" >
+                    <Field/>
+                    <Field/>
                 </div>
+
             </div>
     )
 }
---
+++ client/src/components/Field.jsx
@@ -0,0 +1,10 @@
+function Field() {
+    return (
+        <div className="border pl-2">
+            <h1>Field</h1>
+        </div>
+        
+    )
+}
+
+export default Field;
