a79a6ed9b4f5bec18904c73fd63d1d648060034c
Andres Fernandez <46586316+andyf9903@users.noreply.github.com>
2024-07-09 23:53:01 -0400

    add signup form

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./Home";
 import Header from "./Header";
 import Login from "./Login";
+import Signup from "./Signup";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/home" element={<Home/>}></Route>
           <Route path="/" element={<Login/>}></Route>
+          <Route path="/signup" element={<Signup/>}></Route>
         </Routes>
       </Router>
     </>
---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,43 @@
+function Signup() {
+    return (
+    <div className="flex justify-center items-center max-h-96 mt-16">
+        <div className="bg-secondary p-8 rounded-lg shadow-md h-96 w-[450px] min-w-[350] ">
+          <h1 className="text-2xl text-stone-300 font-bold mb-6 text-center">Sign Up</h1>
+          <form>
+            <div className="mb-4">
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-stone-300">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm focus:outline-none"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-stone-300">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm focus:outline-none"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-stone-100 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
+    )
+}
+
+export default Signup;
